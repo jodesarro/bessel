997342262083a21dce6d9dc656b30646fe036f5e
Jhonas <63566669+jodesarro@users.noreply.github.com>
2020-05-22 11:22:49 -0300

    Create usage_example.cpp

---
+++ usage_example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "bessel.hpp"
+
+using namespace std;
+
+int main()
+{
+    complex<double> z = complex<double> (100, 12) // z=100+i*12
+    cout << bessel::cyl_j0( z );
+}
